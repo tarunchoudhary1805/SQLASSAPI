f0bd1255a767051dde20c7e648007cfd12191267
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2021-04-29 19:17:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const User = require("./questionSchema");
-const port = 8081;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const mongoose = require("mongoose");
 
